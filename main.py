9ec2e1b24dc244293064476d8698be67b6d1a196
edouard-lebas <lebas.edouard@elebas.fr>
2018-04-11 19:01:08 +0200

    make ap

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from src.WifiClass import WifiClass
+# from src.WifiClass import WifiClass
+# w = WifiClass("wlan0")
+# print "AVALABLE => "+str(w.getApAvailable())
+# print "FOUND => "+str(w.searchSSID("toto"))
 
-w = WifiClass("wlan0")
-print "AVALABLE => "+str(w.getApAvailable())
-print "FOUND => "+str(w.searchSSID("Free"))
+from src.MakeAPClass import MakeAPClass
+
+ap = MakeAPClass("22:a3:aa:a0:ba:42", "TEST", 2, "wlan0mon")
+ap.makeAP()
---
+++ src/MakeAPClass.py
@@ -0,0 +1,22 @@
+import subprocess
+
+class MakeAPClass:
+    
+    def __init__(self, mac, essid, chanel, interface):
+        self.mac = mac
+        self.essid = essid
+        self.chanel = chanel
+        self.interface = interface
+
+    def makeAP(self):
+        try:
+            command = "airbase-ng -a "+str(self.mac)+" --essid "+str(self.essid)+" -c "+str(self.chanel)+" "+str(self.interface)
+            proc = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+            out,err = proc.communicate()
+            print out
+            print err
+        except KeyboardInterrupt:
+            print "Interrupt"
+            exit()
+        except:
+            print "Error AP"
