e27faf30df45c282a7b3d4bb96c3fb187e71e3bb
edouard-lebas <lebas.edouard@elebas.fr>
2018-04-12 20:54:20 +0200

    dnsmasq

--- src/DnsMasqClass.py
+++ src/DnsMasqClass.py
@@ -1,8 +1,8 @@
 from src.ExecCommandClass import ExecCommandClass
 class DnsMasqClass:
 
-    def __init__(self, interface, dhcp_min, dhcp_max, dns):
-        self.interface = interface
+    def __init__(self, wirelessinterface, dhcp_min, dhcp_max, dns):
+        self.wirelessinterface = wirelessinterface
         self.dhcp_min = dhcp_min
         self.dhcp_max = dhcp_max
         self.dns = dns
@@ -14,7 +14,7 @@ class DnsMasqClass:
         file.write("# disables dnsmasq reading any other files like /etc/resolv.conf for nameservers\n")
         file.write("no-resolv"+"\n")
         file.write("# Interface to bind to\n")
-        file.write("interface="+str(self.interface)+"\n")
+        file.write("interface="+str(self.wirelessinterface)+"\n")
         file.write("# Specify starting_range,end_range,lease_time\n")
         file.write("dhcp-range="+self.dhcp_min+","+self.dhcp_max+",1h"+"\n")
         file.write("# dns addresses to send to the clients\n")
@@ -23,3 +23,6 @@ class DnsMasqClass:
 
     def startDnsMasq(self):
         self.command.execCommand("dnsmasq")
+    
+    def stopDnsMasq(self):
+        self.command.execCommand("service dnsmasq stop")
